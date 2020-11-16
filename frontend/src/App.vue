fc66325f677fee5800f0a930782780707e957295
KyleAHinton <kyleahinton1@gmail.com>
2020-11-15 23:05:21 -0500

    Formatting changes, addition of a color picker.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,58 @@
 <template>
-  <div>
-    <Editor :canvasWidth="canvasWidth" :canvasHeight="canvasHeight" ref="editor"/>
-    <input id="chooseImage" type="file" @change="uploadImage($event)" accept="image/*"/>
-    <v-btn v-on:click="textMode()">Text Mode</v-btn>
-    <v-btn v-on:click="download()">Download</v-btn>
-
-  </div>
+  <v-app>
+    <v-container>
+      <v-row>
+        <v-col cols="12" sm="2">
+          <v-sheet rounded="lg" min-height="268">
+            <v-color-picker
+              dot-size="25"
+              swatches-max-height="200"
+              hide-canvas
+              hide-inputs
+              show-swatches
+              value="99999"
+              @update:color="Color"
+            ></v-color-picker>
+          </v-sheet>
+        </v-col>
+        <v-col cols="12" sm="8">
+          <v-sheet min-height="70vh" rounded="lg">
+            <Editor
+              :canvasWidth="canvasWidth"
+              :canvasHeight="canvasHeight"
+              ref="editor"
+            />
+          </v-sheet>
+        </v-col>
+        <v-col cols="12" sm="2">
+          <v-sheet rounded="lg" min-height="268">
+            <input
+              id="chooseImage"
+              type="file"
+              @change="uploadImage($event)"
+              accept="image/*"
+            />
+            <v-btn v-on:click="textMode()">Text Mode</v-btn>
+            <v-btn v-on:click="download()">Download Image</v-btn>
+          </v-sheet>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
 </template>
-
 <script>
 import Editor from "vue-image-markup";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
-    Editor
+    Editor,
   },
 
   data() {
     return {
-      color: "black",
+      color: "",
       imageUrl: null,
     };
   },
@@ -42,15 +74,21 @@ export default {
       this.$refs.editor.uploadImage(e);
     },
     textMode() {
-      this.$refs.editor.set('text');
+      this.$refs.editor.set("text");
     },
     download() {
       let link = document.createElement("a");
       link.setAttribute("href", this.$refs.editor.saveImage());
       link.setAttribute("download", "image-markup");
       link.click();
-    }
+    },
+    Color(e) {
+      this.color = e.hex
+      this.$refs.editor.changeColor(this.color);
+    },
   },
 };
-
 </script>
+
+<style scoped>
+</style>
