62a09aaf048e4baf53d05912fd0b919b1d5b4dcb
KyleAHinton <kyleahinton1@gmail.com>
2020-11-15 20:41:53 -0500

    Addition of file upload/editing/downloading through the use of a mark-up library.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,56 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Editor :canvasWidth="canvasWidth" :canvasHeight="canvasHeight" ref="editor"/>
+    <input id="chooseImage" type="file" @change="uploadImage($event)" accept="image/*"/>
+    <v-btn v-on:click="textMode()">Text Mode</v-btn>
+    <v-btn v-on:click="download()">Download</v-btn>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Editor from "vue-image-markup";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    Editor
+  },
+
+  data() {
+    return {
+      color: "black",
+      imageUrl: null,
+    };
+  },
+  props: {
+    canvasWidth: {
+      default: 600,
+    },
+    canvasHeight: {
+      default: 600,
+    },
+  },
+  mounted() {
+    if (this.imageUrl) {
+      this.$refs.editor.setBackgroundImage(this.imageUrl);
+    }
+  },
+  methods: {
+    uploadImage(e) {
+      this.$refs.editor.uploadImage(e);
+    },
+    textMode() {
+      this.$refs.editor.set('text');
+    },
+    download() {
+      let link = document.createElement("a");
+      link.setAttribute("href", this.$refs.editor.saveImage());
+      link.setAttribute("download", "image-markup");
+      link.click();
+    }
+  },
+};
+
+</script>
