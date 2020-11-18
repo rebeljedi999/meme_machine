c18d6e3958e6c9c6a8d247f56a36d115c60e1c9c
KyleAHinton <kyleahinton1@gmail.com>
2020-11-18 10:47:39 -0500

    Addition of routing, to support the swapping of components and site navigation.

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="EditorComponent">
+    <EditorComponent/>
+  </div>
+</template>
+
+<script>
+import EditorComponent from '@/components/EditorComponent.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    EditorComponent
+  }
+}
+</script>
